281d7d7dbeab24c1a2d04f6d3c720c34dbfac645
Jason Maskell <backov@tamedtornado.com>
2026-08-17 11:10:42 +0200

    build: map bundled layer-shell source paths

--- pkg/gtk4-layer-shell/build.zig
+++ pkg/gtk4-layer-shell/build.zig
@@ -145,6 +145,9 @@ fn buildLib(b: *std.Build, module: *std.Build.Module, options: anytype) !*std.Bu
             b.fmt("-DGTK_LAYER_SHELL_MAJOR={}", .{lib_version.major}),
             b.fmt("-DGTK_LAYER_SHELL_MINOR={}", .{lib_version.minor}),
             b.fmt("-DGTK_LAYER_SHELL_MICRO={}", .{lib_version.patch}),
+            b.fmt("-ffile-prefix-map={s}=/usr/src/ghostty/vendor/gtk4-layer-shell", .{
+                upstream.path("").getPath(b),
+            }),
         },
     });
 
